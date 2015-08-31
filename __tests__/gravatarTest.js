c40ee6c9991bbcc8ded13c1045f53d9048484690
Budi Irawan <deerawan@gmail.com>
2015-08-31 22:40:04 +0700

    add params

--- __tests__/gravatarTest.js
+++ __tests__/gravatarTest.js
@@ -1,17 +1,36 @@
 /** @jsx React.DOM */
 jest.dontMock('../src/gravatar.jsx');
 
+var React = require('react/addons');
+var Gravatar = require('../src/gravatar.jsx');
+var TestUtils = React.addons.TestUtils;
+
 describe('Gravatar', function() {
-  it('display correct gravatar image url', function() {
-    var React = require('react/addons');
-    var Gravatar = require('../src/gravatar.jsx');
-    var TestUtils = React.addons.TestUtils;
+  it('should display gravatar image url', function() {
     var gravatar = TestUtils.renderIntoDocument(
       <Gravatar email="deerawan@gmail.com" />
     );
     var image = TestUtils.findRenderedDOMComponentWithTag(
       gravatar, 'img');
     expect(image.getDOMNode().src).toEqual('http://gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0');
+  });
 
+  it('should display gravatar image url with ssl', function() {
+    var gravatar = TestUtils.renderIntoDocument(
+      <Gravatar email="deerawan@gmail.com" ssl={true} />
+    );
+    var image = TestUtils.findRenderedDOMComponentWithTag(
+      gravatar, 'img');
+    expect(image.getDOMNode().src).toEqual('https://secure.gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0');
+  });
+
+  it('should display gravatar image url with all params', function() {
+    var gravatar = TestUtils.renderIntoDocument(
+      <Gravatar email="deerawan@gmail.com" size={200} defaultImage="404" forceDefault="y" rating="pg" />
+    );
+    var image = TestUtils.findRenderedDOMComponentWithTag(
+      gravatar, 'img');
+    expect(image.getDOMNode().src).toEqual(
+      'http://gravatar.com/avatar/71a6523c5915c0ee79a79a137c1b2be0?size=200&default=404&forcedefault=y&rating=pg');
   });
 });
--- src/gravatar.jsx
+++ src/gravatar.jsx
@@ -3,13 +3,54 @@ var React = require('react');
 var md5 = require('md5');
 
 var Gravatar = React.createClass({
+  propTypes: {
+    email: React.PropTypes.string.isRequired,
+    size: React.PropTypes.number,
+    defaultImage: React.PropTypes.string,
+    forceDefault: React.PropTypes.string,
+    rating: React.PropTypes.string,
+    ssl: React.PropTypes.bool
+  },
+  getDefaultProps: function() {
+    return {
+      email: '',
+      ssl: false
+    }
+  },
+  buildParams: function() {
+    var params = [];
+
+    if (this.props.size) {
+      params.push('size=' + this.props.size);
+    }
+    if (this.props.defaultImage) {
+      params.push('default=' + this.props.defaultImage);
+    }
+    if (this.props.forceDefault === 'y') {
+      params.push('forcedefault=' + this.props.forceDefault);
+    }
+    if (this.props.rating) {
+      params.push('rating=' + this.props.rating);
+    }
+
+    return params.join('&');
+  },
+  buildUrl: function() {
+    var url = (this.props.ssl)? 'https://secure.gravatar.com/avatar/' :
+      'http://gravatar.com/avatar/';
+    var urlWithHash = url + md5(this.props.email);
+    var params = this.buildParams();
+    if (params) {
+      urlWithHash += '?' + params;
+    }
+    return urlWithHash;
+  },
   render: function() {
-    var hash = md5(this.props.email);
+    var url = this.buildUrl();
     return (
-      <img src={'http://gravatar.com/avatar/' + hash} />
+      <img {...this.props} src={url} />
     );
   }
 });
 
 module.exports = Gravatar;
-
